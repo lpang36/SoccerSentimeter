172c1750601c687be900827293c695ca1d5a4ea3
lpang36 <lawrencepang36@gmail.com>
2017-07-04 10:39:53 -0400

    create comparison tool

--- base/static/js/dropdown.js
+++ base/static/js/dropdown.js
@@ -1,10 +1,10 @@
 function select (elem,num) {
-	document.getElementById("team"+num).innerText = elem.innerText;
-	document.getElementById("team"+num).setAttribute('class', "comp "+elem.getAttribute('class'));
+	$("#"+num+"dropdown").html(elem.innerText);
+	document.getElementById(num).setAttribute('class', "comp "+elem.getAttribute('class'));
 }
 
 function compare (elem) {
 	str1 = document.getElementById("team1").getAttribute('class').split(" ")[1];
 	str2 = document.getElementById("team2").getAttribute('class').split(" ")[1];
-	elem.setAttribute('href', "{% url 'game' "+str1+" "+str2+" %}");
+	$(elem).attr("href","/game/"+str1+"_"+str2+"/");
 }
